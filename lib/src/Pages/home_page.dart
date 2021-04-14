32a0746195c3a37b05daf6c818678851928051e4
Felix <fcm68247@udelasalle.edu.mx>
2021-04-14 18:35:20 -0500

    v1Login

--- lib/src/Pages/home_page.dart
+++ lib/src/Pages/home_page.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("lib/src/images/Fondo.jpeg"),
-                fit: BoxFit.cover)),
-      ),
-      appBar: AppBar(
-        title: Text('Semestral'),
-      ),
-      //body: ListTile(
-      //  title: Text('Hola'),
-      //  subtitle: Text('Hola2'),
-      // ),
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "lib/src/images/Fondo.jpeg",
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          floatingActionButton: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              new Container(
+                  width: 120.0,
+                  height: 120.0,
+                  decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: new DecorationImage(
+                          fit: BoxFit.fill,
+                          image: new NetworkImage(
+                              "https://www.sosfactory.com/wp-content/uploads/2016/12/mr-bolat-logo.png")))),
+            ],
+          ),
+          body: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.white,
+            ),
+            height: 500,
+            width: 300,
+            margin: const EdgeInsets.all(28),
+            child: new Column(
+              children: <Widget>[TextField(), TextField(), ButtonBar()],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/src/Routes/routes.dart
+++ lib/src/Routes/routes.dart
@@ -1,15 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:semestral_flutter/src/Pages/alert_page.dart';
 import 'package:semestral_flutter/src/Pages/avatar_page.dart';
 import 'package:semestral_flutter/src/Pages/card_page.dart';
 import 'package:semestral_flutter/src/Pages/home_page.dart';
 
-Map <String, WidgetBuilder> getAplicationRoutes(){
+Map<String, WidgetBuilder> getAplicationRoutes() {
   return <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert' : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),
-        'card': (BuildContext context) => CardPage(),
-      };
-  }
+    '/': (BuildContext context) => HomePage(),
+    'alert': (BuildContext context) => AlertPage(),
+    'avatar': (BuildContext context) => AvatarPage(),
+    'card': (BuildContext context) => CardPage(),
+  };
+}
