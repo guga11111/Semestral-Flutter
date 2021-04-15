07987887b5d4e16f95d819cd71a59be20b789b4d
Felix <fcm68247@udelasalle.edu.mx>
2021-04-15 11:05:35 -0500

    1.3 Login

--- lib/src/Pages/card_page.dart
+++ lib/src/Pages/card_page.dart
@@ -16,46 +16,57 @@ class CardPage extends StatelessWidget {
 
   Widget _card() {
     final card = Container(
-        child: Column(children: <Widget>[
-      ListTile(title: Text("")),
-      Column(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.account_circle, color: Colors.black),
-            title: TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Usuario',
+        child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          new Container(
+              width: 120.0,
+              height: 120.0,
+              decoration: new BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image:
+                          new AssetImage("lib/src/images/mr-bolat-logo.png")))),
+          Column(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.account_circle, color: Colors.black),
+                title: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Usuario',
+                  ),
+                ),
               ),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.https, color: Colors.black),
-            title: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Contraseña',
+              ListTile(
+                leading: Icon(Icons.https, color: Colors.black),
+                title: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Contraseña',
+                  ),
+                ),
               ),
-            ),
+              FlatButton(
+                  child: Text('Iniciar sesión'),
+                  color: Colors.orange[400],
+                  textColor: Colors.white,
+                  height: 40,
+                  onPressed: () {}),
+              FlatButton(
+                  child: Text('Registrarse'),
+                  textColor: Colors.orange[400],
+                  color: Colors.white24,
+                  height: 40,
+                  onPressed: () {})
+            ],
           ),
-          FlatButton(
-              child: Text('Iniciar sesión'),
-              color: Colors.orange[400],
-              textColor: Colors.white,
-              height: 60,
-              onPressed: () {}),
-          FlatButton(
-              child: Text('Registrarse'),
-              textColor: Colors.orange[400],
-              color: Colors.white24,
-              height: 60,
-              onPressed: () {})
-        ],
-      ),
-      Container(padding: EdgeInsets.all(5.0), child: Text(''))
-    ]));
+          Container(padding: EdgeInsets.all(5.0), child: Text(''))
+        ]));
 
     return Container(
       margin: const EdgeInsets.all(
