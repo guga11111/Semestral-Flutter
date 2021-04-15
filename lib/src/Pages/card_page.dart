84ed12631207004335667732a93e29a8b8033dcf
Felix <fcm68247@udelasalle.edu.mx>
2021-04-15 00:46:02 -0500

    1.2 Login

--- lib/src/Pages/card_page.dart
+++ lib/src/Pages/card_page.dart
@@ -4,86 +4,62 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.orange[200],
         appBar: AppBar(title: Text('Card Page')),
         body: ListView(children: <Widget>[
-          _cardt1(),
+          _card(),
           SizedBox(
-            height: 30,
-          ),
-          _card2(),
-          SizedBox(
-            height: 30,
-          ),
-          _card3(),
-          SizedBox(
-            height: 30,
+            height: 50,
           ),
         ]));
   }
 
-  Widget _cardt1() {
-    return Card(
+  Widget _card() {
+    final card = Container(
         child: Column(children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.assistant_photo, color: Colors.cyanAccent),
-        title: Text("Inicio de sesión"),
-      ),
-      Row(
+      ListTile(title: Text("")),
+      Column(
         children: <Widget>[
-          FlatButton(child: Text('Aceptar'), onPressed: () {}),
-          FlatButton(child: Text('Aceptar'), onPressed: () {})
+          ListTile(
+            leading: Icon(Icons.account_circle, color: Colors.black),
+            title: TextField(
+              obscureText: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Usuario',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.https, color: Colors.black),
+            title: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Contraseña',
+              ),
+            ),
+          ),
+          FlatButton(
+              child: Text('Iniciar sesión'),
+              color: Colors.orange[400],
+              textColor: Colors.white,
+              height: 60,
+              onPressed: () {}),
+          FlatButton(
+              child: Text('Registrarse'),
+              textColor: Colors.orange[400],
+              color: Colors.white24,
+              height: 60,
+              onPressed: () {})
         ],
-      )
-    ]));
-  }
-
-  Widget _card2() {
-    final card = Container(
-        child: Column(children: <Widget>[
-      FadeInImage(
-          image: NetworkImage(
-              'https://cdn.pixabay.com/photo/2021/01/21/17/58/herbstastern-5938056_1280.jpg'),
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          fadeInDuration: Duration(microseconds: 500),
-          height: 200.0,
-          fit: BoxFit.cover),
-      Container(
-          padding: EdgeInsets.all(5.0), child: Text('Descripcion de la imagen'))
-    ]));
-
-    return Container(
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: card,
       ),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.0),
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10,
-                spreadRadius: 10,
-                offset: Offset(2.0, 10.0))
-          ],
-          color: Colors.white),
-    );
-  }
-
-  Widget _card3() {
-    final card = Container(
-        child: Column(children: <Widget>[
-      FadeInImage(
-          image: NetworkImage(
-              'https://cdn.pixabay.com/photo/2021/01/21/17/58/herbstastern-5938056_1280.jpg'),
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          fadeInDuration: Duration(microseconds: 500),
-          height: 200.0,
-          fit: BoxFit.cover),
-      Container(
-          padding: EdgeInsets.all(5.0), child: Text('Descripcion de la imagen'))
+      Container(padding: EdgeInsets.all(5.0), child: Text(''))
     ]));
 
     return Container(
+      margin: const EdgeInsets.all(
+          30), //Guga, si no te sale centrado es por esto xd
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20.0),
         child: card,
