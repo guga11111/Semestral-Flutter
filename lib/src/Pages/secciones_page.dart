f92806e1525af5998c4a4287d91af2aab262c3bf
Gustavo <guga.nd@hotmail.com>
2021-04-15 22:16:58 -0500

    Secciones cargadas

--- lib/src/Pages/secciones_page.dart
+++ lib/src/Pages/secciones_page.dart
@@ -8,6 +8,8 @@ class SeccionesPage extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.orange[200],
         body: ListView(children: <Widget>[
+       
+          _card1(context),
           _card(context),
           SizedBox(
             height: 50,
@@ -20,26 +22,127 @@ class SeccionesPage extends StatelessWidget {
         child: new Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
+            
             children: <Widget>[
-
-          Column(
+              
+          Row(
             children: <Widget>[
               FlatButton(
-                  child: Image.asset('lib/src/images/pozole_acapulco.jpg'),
+                  child: Image.asset('lib/src/images/Caldos.png'),
                   height: 30,
                   textColor: Colors.orange[400],
                   color: Colors.white24,
+                  padding: const EdgeInsets.only(
+                  left: 13,
+                  top: 15
+                  ),
+
+                  onPressed: () {
+                    _navigateToNextScreen(context);
+                  }),
+                  FlatButton(
+                  child: Image.asset('lib/src/images/Plato fuerte.png'),
+                  height: 30,
+                  textColor: Colors.orange[400],
+                  color: Colors.white24,
+                  padding: const EdgeInsets.only(
+                  left: 15,
+                  top: 15
+                  ),
                   onPressed: () {
                     _navigateToNextScreen(context);
                   })
             ],
           ),
-          Container(padding: EdgeInsets.all(5.0), child: Text(''))
+          Row(
+            children: <Widget>[
+              FlatButton(
+                  child: Image.asset('lib/src/images/Postres.png'),
+                  height: 30,
+                  textColor: Colors.orange[400],
+                  color: Colors.white24,
+                  padding: const EdgeInsets.only(
+                  left: 15, top: 10
+                  ),
+
+                  onPressed: () {
+                    _navigateToNextScreen(context);
+                  }),
+                  FlatButton(
+                  child: Image.asset('lib/src/images/Especial.png'),
+                  height: 30,
+                  textColor: Colors.orange[400],
+                  color: Colors.white24,
+                  padding: const EdgeInsets.only(
+                  left: 15,
+                  top: 15
+                  ),
+                  onPressed: () {
+                    _navigateToNextScreen(context);
+                  })
+            ],
+          ),
+          Row(
+            
+            children: <Widget>[
+              FlatButton(
+                  child: Image.asset('lib/src/images/Comida rápida.png'),
+                  height: 30,
+                  textColor: Colors.orange[400],
+                  color: Colors.white24,
+                  padding: const EdgeInsets.only(
+                  left: 15, top: 10
+                  ),
+                  onPressed: () {
+                    _navigateToNextScreen(context);
+                  }),
+                  FlatButton(
+                  child: Image.asset('lib/src/images/Bebidas alcoholicas.png'),
+                  height: 30,
+                  textColor: Colors.orange[400],
+                  color: Colors.white24,
+                  padding: const EdgeInsets.only(
+                  left: 15,
+                  top: 15
+                  ),
+                  onPressed: () {
+                    _navigateToNextScreen(context);
+                  })
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              FlatButton(
+                  child: Image.asset('lib/src/images/Refrescos.png'),
+                  height: 30,
+                  textColor: Colors.orange[400],
+                  color: Colors.white24,
+                  padding: const EdgeInsets.only(
+                  left: 15, top: 10
+                  ),
+
+                  onPressed: () {
+                    _navigateToNextScreen(context);
+                  }),
+                  FlatButton(
+                  child: Image.asset('lib/src/images/Souvenirs.png'),
+                  height: 30,
+                  textColor: Colors.orange[400],
+                  padding: const EdgeInsets.only(
+                  left: 15,
+                  top: 15
+                  ),
+                  onPressed: () {
+                    _navigateToNextScreen(context);
+                  })
+            ],
+          ),
+          Container(padding: EdgeInsets.all(4.0), child: Text(''))
         ]));
 
     return Container(
-      margin: const EdgeInsets.all(
-          30), //Guga, si no te sale centrado es por esto xd
+     padding: EdgeInsets.only(left: 2, right: 10),
+      margin: EdgeInsets.only(left: 30, right: 30, top: 0),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20.0),
         child: card,
@@ -53,7 +156,56 @@ class SeccionesPage extends StatelessWidget {
                 spreadRadius: 10,
                 offset: Offset(2.0, 10.0))
           ],
+          
           color: Colors.white),
+    );
+  }
+
+  Widget _card1(BuildContext context) {
+    final card = Container(
+                  padding: const EdgeInsets.only(
+                  top: 0,
+                  bottom: 0
+                  ),
+        child: new Column(
+            
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          Row(
+            
+            children: <Widget>[
+               FlatButton(
+                  child: Text('Menu'),
+                  textColor: Colors.black,
+                  height: 40,
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => CarritoPage()));
+                  }),
+                FlatButton(
+                  child: Text('Pedidos'),
+                  textColor: Colors.black,
+                  height: 40,
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => CarritoPage()));
+                  }),
+            ],
+          ),
+          Container(padding: EdgeInsets.all(0.0), child: Text(''))
+        ]));
+
+    return Container(
+    //Guga, si no te sale centrado es por esto xd
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: card,
+      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+         
+          ),
     );
   }
 
